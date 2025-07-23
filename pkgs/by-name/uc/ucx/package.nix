9d047c8cc130a25b7b122474985f24ee2a21d688
qbisi <qbisicwate@gmail.com>
2025-07-23 20:54:21 +0800

    ucx: specify meta.platforms (not upstreamed)

--- pkgs/by-name/uc/ucx/package.nix
+++ pkgs/by-name/uc/ucx/package.nix
@@ -106,11 +106,14 @@ stdenv'.mkDerivation rec {
 
   enableParallelBuilding = true;
 
-  meta = with lib; {
+  meta = {
     description = "Unified Communication X library";
     homepage = "https://www.openucx.org";
-    license = licenses.bsd3;
-    platforms = platforms.linux;
-    maintainers = [ maintainers.markuskowa ];
+    license = lib.licenses.bsd3;
+    platforms = with lib.platform; [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    maintainers = with lib.maintainers; [ markuskowa ];
   };
 }
