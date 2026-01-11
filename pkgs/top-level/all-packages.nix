134d27dea62a633750ce6354b2d39016047c3e3d
qbisi <qbisicwate@gmail.com>
2026-01-12 01:32:45 +0800

    luajit_loongson: init at 2.1.1749125925

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4745,6 +4745,7 @@ with pkgs;
     luajit_2_1
     luajit_2_0
     luajit_openresty
+    luajit_loongson
     ;
 
   lua5 = lua5_2_compat;
@@ -4759,7 +4760,7 @@ with pkgs;
 
   luaPackages = lua52Packages;
 
-  luajit = luajit_2_1;
+  luajit = if stdenv.hostPlatform.isLoongArch64 then luajit_loongson else luajit_2_1;
 
   luarocks = luaPackages.luarocks;
   luarocks-nix = luaPackages.luarocks-nix;
