9d12fa2eccb15ce44143fe9560f769bec4b931fb
Weijia Wang <9713184+wegank@users.noreply.github.com>
2026-09-05 18:41:01 +0200

    openjdk: rebootstrap on loongarch64-linux

--- pkgs/top-level/java-packages.nix
+++ pkgs/top-level/java-packages.nix
@@ -56,18 +56,21 @@ in
       corretto21 = callPackage ../development/compilers/corretto/21.nix { };
       corretto25 = callPackage ../development/compilers/corretto/25.nix { };
 
-      # TODO: compile OpenJDK for LoongArch
-      openjdk8 = if stdenv.hostPlatform.isLoongArch64 then loongson-jdk-bin.jdk-8 else mkOpenjdk "8";
-      openjdk11 = if stdenv.hostPlatform.isLoongArch64 then loongson-jdk-bin.jdk-11 else mkOpenjdk "11";
-      openjdk17 = if stdenv.hostPlatform.isLoongArch64 then loongson-jdk-bin.jdk-17 else mkOpenjdk "17";
-      openjdk21 = if stdenv.hostPlatform.isLoongArch64 then loongson-jdk-bin.jdk-21 else mkOpenjdk "21";
-      openjdk25 = if stdenv.hostPlatform.isLoongArch64 then loongson-jdk-bin.jdk-25 else mkOpenjdk "25";
+      openjdk8 = mkOpenjdk "8";
+      openjdk11 = mkOpenjdk "11";
+      openjdk17 = mkOpenjdk "17";
+      openjdk21 = mkOpenjdk "21";
+      openjdk25 = mkOpenjdk "25";
 
       # Legacy aliases
-      # TODO: LoongArch
-      openjdk8-bootstrap = temurin-bin.jdk-8;
-      openjdk11-bootstrap = temurin-bin.jdk-11;
-      openjdk17-bootstrap = temurin-bin.jdk-17;
+      # Temurin publishes no LoongArch64 binaries; use Loongson's prebuilt JDKs
+      # to seed the bootstrap chain there instead.
+      openjdk8-bootstrap =
+        if stdenv.hostPlatform.isLoongArch64 then loongson-jdk-bin.jdk-8 else temurin-bin.jdk-8;
+      openjdk11-bootstrap =
+        if stdenv.hostPlatform.isLoongArch64 then loongson-jdk-bin.jdk-11 else temurin-bin.jdk-11;
+      openjdk17-bootstrap =
+        if stdenv.hostPlatform.isLoongArch64 then loongson-jdk-bin.jdk-17 else temurin-bin.jdk-17;
 
       temurin-bin = lib.recurseIntoAttrs (
         let
